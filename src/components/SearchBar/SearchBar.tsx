0c620b21e333c8afc297824f11d1888af6896d56
Ivan Voddz <voddzanovskij@gmail.com>
2026-03-29 14:02:43 +0200

    fix

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -6,18 +6,15 @@ interface SearchBarProps {
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const query = (
-      form.elements.namedItem("query") as HTMLInputElement
-    ).value.trim();
+  const handleAction = (formData: FormData) => {
+    const query = (formData.get("query") as string).trim();
+
     if (!query) {
       toast.error("Please enter your search query.");
       return;
     }
+
     onSubmit(query);
-    form.reset();
   };
 
   return (
@@ -31,7 +28,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
         >
           Powered by TMDB
         </a>
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form className={styles.form} action={handleAction}>
           <input
             className={styles.input}
             type="text"
